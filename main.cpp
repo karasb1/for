0868aaa7c558445975a59170a76bfeb631af7736
karasb1 <bohdankaras456@gmail.com>
2024-05-11 16:32:18 +0300

    .

--- main.cpp
+++ main.cpp
@@ -4,5 +4,18 @@ using namespace std;
 
 int main()
 {
+	int a = 0, b = 0, c = 0, k = 0, r = 0;
+	cin >> a;
+	int r = a;
+	cout r;
+	for (;r<10;r/10) {
+		k++;
+	}
+	k++;
+	cout << k;
+
+
+
+
 	return 0;
 }
