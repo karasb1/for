9556cf9ffc172c314951aca0be4c6c230afb7ae7
karasb1 <bohdankaras456@gmail.com>
2024-05-11 15:56:15 +0300

    The ex2 was done.

--- main.cpp
+++ main.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main()
 {
+	int k = 0;
+	for (int i = 100; i <= 999; i++) {
+		if (i / 100 != i % 100 / 10 && i / 100 != i % 10 && i % 100 / 10 != i / 100) {
+			k++;
+		}
+	}
+	cout << k << endl;
 	return 0;
 }
