588f17b13c7660c1d2522650a2d5946c612ba390
karasb1 <bohdankaras456@gmail.com>
2024-05-11 17:17:16 +0300

    The ex1 was done.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
-#include <Windows.h>
+#include <ctime>
+#include <chrono>
 using namespace std;
 
-int main()
-{
+int main(){
+	srand((unsigned int)time(0));
+	int number = rand() % 500 + 1;
+	int a = 0, k = 0;
+	for (;;) {
+		cin >> a;
+		if (a == number) {
+			cout << "You guessed it " << ++k << endl;
+			break;
+		}
+		if (a < number) {
+			cout << "More" << endl;
+			k++;
+		}
+		if (a > number) {
+			cout << "Less" << endl;
+			k++;
+		}
+		if (a == 0) {
+			cout << "Game over" << endl;
+		}
+	}
 	return 0;
 }
