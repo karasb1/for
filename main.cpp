12eb3191c562208ef56e93c043da9995cc115d38
karasb1 <bohdankaras456@gmail.com>
2024-09-08 17:37:22 +0300

    The ex5 was done.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
-#include <Windows.h>
+#include <cmath>
 using namespace std;
 
 int main()
 {
+	int a, s = 0;
+	cin >> a;
+	for (int b = a; b > 0; b /= 10 ) {
+		s += b % 10;
+	}
+	if (s * s * s = a * a) {
+		cout << "true" << endl;
+	}
+	else {
+		cout << "false" << endl;
+	}
 	return 0;
 }
