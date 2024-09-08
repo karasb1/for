6b896483722a2e6eef3dc24618e50acc95e849d7
karasb1 <bohdankaras456@gmail.com>
2024-09-08 17:21:17 +0300

    The ex4 was done.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-#include <Windows.h>
 using namespace std;
 
 int main()
 {
+	int a;
+	cin >> a;
+	for (int b = 0; b <= a; b++) {
+		if (a % (b * b) == 0 && a % (b * b * b) != 0) {
+			cout << b << " ";
+		}
+	}
+	cout << endl;
 	return 0;
 }
