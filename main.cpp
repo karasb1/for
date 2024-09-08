c02074adb3c2842e39394b7fab7487f27bb72375
karasb1 <bohdankaras456@gmail.com>
2024-09-08 21:42:34 +0300

    The ex7 was done.

--- main.cpp
+++ main.cpp
@@ -4,5 +4,18 @@ using namespace std;
 
 int main()
 {
+	int a, b, c;
+	cin >> a;
+	cin >> b;
+	if (b > a) {
+		c = b;
+		b = a;
+		a = c;
+	}
+	for (int i = 1; i <= b; i++) {
+		if (a % i == 0 && b % i == 0) {
+			cout << i << " ";
+		}
+	}
 	return 0;
 }
